3d388a12ebbc708bedf4467f10ed5c7f8afc6929
Shiizuo <slicedheaven314@gmail.com>
2022-12-04 12:52:15 +0530

    board.py
    Created board module. VS Code shows some 'syntax error jedi' but code works fine.

---
+++ src/board.py
@@ -0,0 +1,13 @@
+from const import *
+from square import Square
+
+class Board:
+
+    def __init__(self):
+        self.squares = [[0 for i in range(cols)]for j in range(rows)]
+        self.createBoard()
+
+    def createBoard(self):
+        for row in rows:
+            for col in cols:
+                self.squares[row][col] = Square(row,col)
