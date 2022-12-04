0a3a3dc7edab2cd0a720d1f8369bbcf19a78cedf
Shiizuo <slicedheaven314@gmail.com>
2022-12-04 11:33:14 +0530

    Initial commit
    Readme, const, main file; game map

---
+++ README.md
@@ -0,0 +1,5 @@
+# TIC-TAC-TOE
+
+Made by:- 
+Arjun Singh XII-D
+Yash Verma XII-E
---
+++ src/const.py
@@ -0,0 +1,13 @@
+# screen
+length = 400
+height = 600
+
+# menu
+menuLength = length
+menuHeight = height - length
+
+# board
+boardLength = boardHeight = length
+rows = 4
+cols = 4
+sqSize = length//cols
